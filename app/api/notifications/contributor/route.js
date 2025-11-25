250ee6d61ba4f7c534670b4234155eaad67543c1
Roberto <ruuby2602@gmail.com>
2025-11-25 10:27:57 -0600

    ya3

--- app/api/notifications/contributor/route.js
+++ app/api/notifications/contributor/route.js
@@ -4,6 +4,8 @@ import connectMongo from "@/libs/mongoose";
 import ArticleSuggestion from "@/models/ArticleSuggestion";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export async function GET(request) {
   try {
